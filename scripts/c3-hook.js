f58957d1db39d740759c6e646ef3784161314d6a
kapiokapitoken-glitch <kapiokapitoken@gmail.com>
2025-09-17 03:36:48 +0300

    debug: early hook Eb.Runtime.GetJsPropName via window.Eb setter

--- scripts/c3-hook.js
+++ scripts/c3-hook.js
@@ -1,53 +1,67 @@
 ﻿(function(){
-  // Güvenli log helper
-  function safeLog(){ try{ console.log.apply(console, arguments); }catch(_){} }
-  function safeErr(){ try{ console.error.apply(console, arguments); }catch(_){} }
+  function slog(){ try{ console.log.apply(console, arguments); }catch(_){} }
+  function serr(){ try{ console.error.apply(console, arguments); }catch(_){} }
 
-  function hookOnce(){
+  function hookGPN(Eb){
     try{
-      var Eb = window.Eb;
-      if (!Eb || !Eb.Runtime) return false;
-
-      // 1) *** ÖNEML ***: Stack'te görünen fonksiyon sınıf üstünde -> Eb.Runtime.GetJsPropName
-      if (typeof Eb.Runtime.GetJsPropName === "function" && !Eb.Runtime.__gpnHooked){
-        var _origGPN = Eb.Runtime.GetJsPropName;
-        Eb.Runtime.GetJsPropName = function(){
+      if(!Eb || !Eb.Runtime) return;
+      if(Eb.Runtime.__gpnHooked) return;
+      var orig = Eb.Runtime.GetJsPropName;
+      if(typeof orig !== "function") return;
+      Eb.Runtime.GetJsPropName = function(){
+        try { return orig.apply(this, arguments); }
+        catch(e){
           try{
-            return _origGPN.apply(this, arguments);
-          }catch(e){
-            safeErr("[C3-HOOK] GetJsPropName ERROR",
-                    "args=", Array.prototype.slice.call(arguments),
-                    "message=", (e && e.message) || e);
-            throw e;
-          }
-        };
-        Eb.Runtime.__gpnHooked = true;
-        safeLog("[C3-HOOK] Hooked Eb.Runtime.GetJsPropName");
-      }
-
-      // 2) EventVariable.Create mevcutsa isimleri logla (minify adına göre iki olası yol)
-      var EvVarClass = (window.PG && PG.EventVariable) || (window.gG && gG.EventVariable);
-      if (EvVarClass && EvVarClass.prototype && typeof EvVarClass.prototype.Create === "function" && !EvVarClass.prototype.__evCreateHooked){
-        var _origCreate = EvVarClass.prototype.Create;
-        EvVarClass.prototype.Create = function(){
-          try{
-            var name = (this && (this.n || this.name)) || "(unknown)";
-            safeLog("[C3-HOOK] EventVariable.Create name=", name);
+            serr("[C3-HOOK] GetJsPropName ERROR",
+                 "args=", Array.prototype.slice.call(arguments),
+                 "message=", (e && e.message) || e);
           }catch(_){}
-          return _origCreate.apply(this, arguments);
-        };
-        EvVarClass.prototype.__evCreateHooked = true;
-        safeLog("[C3-HOOK] Hooked EventVariable.Create");
-      }
-
-      return true;
-    }catch(_){
-      return false;
-    }
+          throw e;
+        }
+      };
+      Eb.Runtime.__gpnHooked = true;
+      slog("[C3-HOOK] Hooked Eb.Runtime.GetJsPropName (early)");
+    }catch(e){/*noop*/}
   }
 
-  // C3 kodu gelene kadar periyodik dene
+  // 1) Mevcut Eb varsa hemen kanca
+  if (window.Eb) hookGPN(window.Eb);
+
+  // 2) Eb atanınca otomatik kanca: setter
+  try{
+    var __Eb = window.Eb;
+    Object.defineProperty(window, "Eb", {
+      configurable: true,
+      enumerable: true,
+      get(){ return __Eb; },
+      set(v){ __Eb = v; try{ hookGPN(v); }catch(_){} }
+    });
+    slog("[C3-HOOK] window.Eb setter installed");
+  }catch(e){
+    serr("[C3-HOOK] defineProperty failed", e);
+  }
+
+  // (Opsiyonel) EventVariable.Create isimlerini yazdır
+  function tryHookEventVar(){
+    try{
+      var EvVar = (window.PG && PG.EventVariable) || (window.gG && gG.EventVariable);
+      if(!EvVar || !EvVar.prototype || typeof EvVar.prototype.Create !== "function" || EvVar.prototype.__evHooked) return;
+      var orig = EvVar.prototype.Create;
+      EvVar.prototype.Create = function(){
+        try{
+          var name = (this && (this.n || this.name)) || "(unknown)";
+          slog("[C3-HOOK] EventVariable.Create name=", name);
+        }catch(_){}
+        return orig.apply(this, arguments);
+      };
+      EvVar.prototype.__evHooked = true;
+      slog("[C3-HOOK] Hooked EventVariable.Create");
+    }catch(_){}
+  }
+
+  // PG/gG sonradan gelirse periyodik dene
   var t = setInterval(function(){
-    if (hookOnce()){ clearInterval(t); }
-  }, 80);
+    tryHookEventVar();
+    if (window.Eb && window.Eb.Runtime && window.Eb.Runtime.__gpnHooked) { /* bırak devam etsin */ }
+  }, 60);
 })();
